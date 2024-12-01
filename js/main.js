258ee2e27b3099bedf1e9875a0566376e79b63b2
Gonzalo Bressan <bressangonzalo2004@hotmail.com>
2024-12-01 20:47:21 -0300

    LiveListV1.0

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 let task_list = JSON.parse(localStorage.getItem("task_list")) || [];
 
+let completed_tasks = JSON.parse(localStorage.getItem("completed_tasks")) || [];
+
 let addBtn = document.getElementById("addBtn");
 
 let taskCounter = document.getElementById("taskCounter");
 
 let taskCounterContainer = document.getElementById("taskCounterContainer")
+
+let deleteHistoryBtn = document.getElementById("deleteHistoryBtn");
 
 class task{
 
@@ -13,6 +17,8 @@ class task{
     }
 
 }
+
+
 
 
 function addTask() {
@@ -35,11 +41,23 @@ function addTask() {
         
 
     }else{
-        alert("Debes ingresar un titulo para agregar una tarea");
+        swal.fire({
+            title: "ingresa una tarea",
+            text: "Debes ingresar un titulo para crear una tarea, sino como sabes lo que tienes que hacer?",
+            icon: "question",
+        })
     }
 }
+if (addBtn){
 
-addBtn.addEventListener("click",addTask);
+    addBtn.addEventListener("click",addTask);
+
+}
+if (deleteHistoryBtn){
+
+    deleteHistoryBtn.addEventListener("click", clearHistory);
+
+}
 
 taskInput.addEventListener("keypress", (event) => {
     if(event.key === "Enter"){
@@ -77,6 +95,9 @@ function getTaskList(){
 
 function deleteTask(nroTask) {
 
+    completed_tasks.push(task_list[nroTask]);
+    saveCompletedTasks();
+
     task_list.splice(nroTask,1);
 
     saveTasks();
@@ -84,6 +105,10 @@ function deleteTask(nroTask) {
     getTaskList();
     
     getTaskCounter();
+}
+
+function saveCompletedTasks() {
+    localStorage.setItem("completed_tasks", JSON.stringify(completed_tasks));
 }
 
 function saveTasks() {
@@ -96,6 +121,70 @@ function getTaskCounter() {
     taskCounter.textContent = `Tareas pendientes: ${task_list.length}`;
     taskCounterContainer.appendChild(taskCounter);
 }
+
+function getTaskHistoryList(){
+
+    let taskHistoryList = document.getElementById("taskHistoryList");
+    taskHistoryList.innerHTML = "";
+
+
+    completed_tasks.reverse().forEach((task, nroTask)=> {
+        let taskElement = document.createElement("li");
+        taskElement.className = "task";
+
+        let taskName = document.createElement("p");
+        taskName.textContent = `${task.title} - ${task.time}`;
+
+        taskElement.appendChild(taskName);
+        taskHistoryList.appendChild(taskElement);
+    });
+
+    updateHistoryCounters();
+}
+
+function updateHistoryCounters() {
+    
+    document.querySelector(".finishedTaskHistoryCounter").textContent = `Todas las tareas finalizadas: ${completed_tasks.length}`;
+    document.querySelector(".UnfinishedTaskHistoryCounter").textContent = `Tareas sin terminar: ${task_list.length}`;
+    
+}
+
+if (deleteHistoryBtn){
+    
+    deleteHistoryBtn.addEventListener("click", clearHistory);
+
+}
+
+function clearHistory() {
+    swal.fire({
+        title: "¿Estás seguro?",
+        text: "Esto eliminará todas las tareas completadas de forma permanente.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+
+
+        if (result.isConfirmed) {
+
+            completed_tasks = [];
+            saveCompletedTasks();
+
+            getTaskHistoryList();
+
+
+            swal.fire({
+                title: "Eliminado",
+                text: "El historial de tareas ha sido eliminado.",
+                icon: "success",
+            });
+        }
+    });
+}
+
 
 
 getTaskList();
